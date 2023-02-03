0aded996f06094932488abb3efd06b0f6ee35323
Y.V.Rohith <110025038+RohithVY@users.noreply.github.com>
2023-02-03 20:57:13 +0530

    intial server setup

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`)
+})
