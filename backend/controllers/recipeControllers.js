b025f0510da78e497bf935d7f603d4c591016bc6
Y.V.Rohith <110025038+RohithVY@users.noreply.github.com>
2023-02-03 21:59:55 +0530

    get particular item using id, put and delete methods were updated

--- backend/controllers/recipeControllers.js
+++ backend/controllers/recipeControllers.js
@@ -22,16 +22,38 @@ const postRecipes = asyncHandler( async (req, res) => {
 })
 
 const editRecipes = asyncHandler( async (req, res) => {
-    res.json({message : `put request executed using ${req.params.id}`})
+    const { id } = req.params;
+    const { name, toppings, price } = req.body;
+
+    const item = itemData.find((item) => item.id === id);
+
+    if (name) item.name = name;
+    if (toppings) item.toppings = toppings;
+    if (price) item.price = price;
+
+    res.send(
+        `Item with id ${id} and name ${item.name} has been updated successfully`
+    );
 })
 
 const deleteRecipes = asyncHandler( async (req, res) => {
-    res.json({message : `delete request executed using ${req.params.id}`})
+    const { id } = req.params;
+
+    itemData = itemData.filter((item) => item.id !== id);
+    res.send(`Item with id ${id} has been deleted successfully.`);
+})
+
+const getParticularRecipe = asyncHandler( async(req, res) => {
+    const { id } = req.params;
+
+    const itemFound = itemData.find((item) => item.id === id);
+    res.json(itemFound)
 })
 
 module.exports = {
     fetchRecipes,
     postRecipes,
     editRecipes,
-    deleteRecipes
+    deleteRecipes,
+    getParticularRecipe
 }
--- backend/routers/receipeRoutes.js
+++ backend/routers/receipeRoutes.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const { uid } = require("uuidv4")
 
-const { fetchRecipes, postRecipes, editRecipes, deleteRecipes } = require('../controllers/recipeControllers')
+const { fetchRecipes, postRecipes, editRecipes, deleteRecipes, getParticularRecipe } = require('../controllers/recipeControllers')
 
 router.get('/',fetchRecipes)
+
+router.get('/:id',getParticularRecipe)
 
 router.post('/', postRecipes)
 
